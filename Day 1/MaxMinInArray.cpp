c85e21aade32eb20b9cf6e7ad0b236174b54278c
pranshu4302 <143099469+pranshu4302@users.noreply.github.com>
2023-09-01 13:13:19 +0530

    Create MaxMinInArray.cpp

---
+++ Day 1/MaxMinInArray.cpp
@@ -0,0 +1,13 @@
+class Solution
+{
+   public:
+    int findSum(int A[], int N)
+    {
+    	sort(A.begin(),A.end());
+    	for(int i=0;i<N;i++){
+    	    cout<<A[0]<<" first elsement of the array";
+    	    cout<<A[n-1]<<" last elsement"
+    	}
+    }
+
+};
