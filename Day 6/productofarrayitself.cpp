bdbfba55f0dfc6ca93b43780229ae0751b089b10
pranshu4302 <143099469+pranshu4302@users.noreply.github.com>
2023-09-06 11:52:56 +0530

    Create productofarrayitself.cpp

---
+++ Day 6/productofarrayitself.cpp
@@ -0,0 +1,29 @@
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n =nums.size();
+        vector<int> pre(n);
+        vector<int> suf(n);
+        vector<int> ans(n);
+        // prefix product array
+        int p=nums[0];
+        pre[0]=1;
+        for(int i=1;i<n;i++){
+            pre[i]=p;
+            p*=nums[i];
+        }
+        // suffix product arrary
+        p=nums[n-1];
+        suf[n-1]=1;
+         for(int i=n-2;i>=0;i--){
+            suf[i]=p;
+            p*=nums[i];
+        }
+        // ans array
+         for(int i=0;i<n;i++){
+            ans[i]=pre[i]*suf[i];
+        }
+        return ans;
+
+
+    }
+};
