bb4c982d777d34a118f1a06d9ea999135b9bcc6e
pranshu4302 <143099469+pranshu4302@users.noreply.github.com>
2023-09-08 10:43:05 +0530

    Create givensumpair.cpp

---
+++ Day 8/givensumpair.cpp
@@ -0,0 +1,18 @@
+class Solution{   
+public:
+    int getPairsCount(int arr[], int n, int k) {
+        // code here
+           int ans = 0;
+        map<int,int>m;
+        m[arr[0]]++;
+        for(int i=1;i<n;i++)
+        {
+            if(arr[i]<k)
+            {
+                ans+=m[k-arr[i]];
+                m[arr[i]]++;
+            }
+        }
+        return ans;
+    }
+};
